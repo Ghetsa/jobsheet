39f3244073d580bb6fb7b57f1307e5287f845857
Ghetsa <ghetsa.arryanti@gmail.com>
2023-09-26 18:56:42 +0700

    modifikasi PemilihanPercobaan13.java

--- jobsheet5/PemilihanPercobaan113.java
+++ jobsheet5/PemilihanPercobaan113.java
@@ -8,13 +8,10 @@ public class PemilihanPercobaan113 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
         System.out.print("masukkan angka: ");
+        String hasil;
         int angka = input13.nextInt();
 
-
-        if (angka % 2 == 0) {
-            System.out.println("Angka "+angka+ " bilangan genap");
-        } else {
-            System.out.println("Angka "+angka+ " bilangan ganjil");
+        hasil = (angka % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
+            System.out.println("Angka " + angka + " adalah " + hasil);
         }
-    }
 }
