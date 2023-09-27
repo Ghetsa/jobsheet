9ffef4f2fda36fb40a847ffda3056d30633d6847
Ghetsa <ghetsa.arryanti@gmail.com>
2023-09-27 07:56:46 +0700

    Modifikasi PemilihanPercobaan212.java

--- jobsheet5/PemilihanPercobaan213.java
+++ jobsheet5/PemilihanPercobaan213.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PemilihanPercobaan213 {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
+        String message;
 
         System.out.print("Nilai uas       : ");
         float uas = input13.nextFloat();
@@ -13,10 +14,30 @@ public class PemilihanPercobaan213 {
         System.out.print("Nilai tugas     : ");
         float kuis = input13.nextFloat();
 
-        float total = (uas * 0.4F) + (uts * 0.1F) + (kuis * 0.1F) + (tugas * 0.2F);
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
-        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        if (total > 80 && total <= 100) {
+            System.out.println(message="A" );
+        } else if (total > 73 && total <= 80) {
+            System.out.println(message="Anda mendapatkan nilai B+" );
+        } else if (total > 65 && total <= 73) {
+            System.out.println(message="Anda mendapatkan nilai B" );
+        } else if (total > 60 && total <= 65) {
+            System.out.println(message="Anda mendapatkan nilai C+" );
+        } else if (total > 50 && total <= 60) {
+            System.out.println(message="Anda mendapatkan nilai C" );
+        } else if (total > 39 && total <= 50) {
+            System.out.println(message="Anda mendapatkan nilai D" );
+        } else if (total >= 0 && total <= 39) {
+            System.out.println(message="Anda mendapatkan nilai E" );
+        } else {
+            System.out.println(message="Nilai yang anda masukkan tidak valid" );
+        }
 
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+
+        String hasil = total < 65 ? "Remidi" : "Tidak remidi";
+
+
+        System.out.println("Nilai akhir = " + total + " sehingga " + message + " Maka " + hasil);
     }
 }
