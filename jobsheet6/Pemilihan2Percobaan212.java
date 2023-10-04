90709a551d156bce842efe9ed71b7aefad41ef25
Ghetsa <ghetsa.arryanti@gmail.com>
2023-10-04 08:59:55 +0700

    fix Pemilihan2Percobaan212.java setelah modifikasi

--- jobsheet6/Pemilihan2Percobaan212.java
+++ jobsheet6/Pemilihan2Percobaan212.java
@@ -23,10 +23,10 @@ public class Pemilihan2Percobaan212 {
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            } else if ((sudut1 < 90) && (sudut2 < 90) && (sudut3 < 90)) {
-                System.out.println("Segitiga tersebut adalah segitiga lancip");
+            } else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3)) {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
             } else {
-                System.out.println("Segitiga tersebut adalah segitiga tumpul");
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
             }
         } else {
             System.out.println("Bukan Segitiga");
