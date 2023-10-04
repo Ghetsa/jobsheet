b6c84a71457e090efda427a7c10904f38ec39440
Ghetsa <ghetsa.arryanti@gmail.com>
2023-10-04 07:15:07 +0700

    Pemilihan2Percobaan112.java setelah modifikasi no4

--- jobsheet6/Pemilihan2Percobaan112.java
+++ jobsheet6/Pemilihan2Percobaan112.java
@@ -14,10 +14,14 @@ public class Pemilihan2Percobaan112 {
         tahun = input12.nextInt();
 
         if (tahun % 4 == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             } else {
-                System.out.println("Bukan Tahun Kabisat");
+                System.out.println("Tahun Kabisat");
             }
         } else {
             System.out.println("Bukan Tahun Kabisat");
