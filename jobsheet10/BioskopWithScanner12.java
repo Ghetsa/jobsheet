b6e707fddee8a3009be7fb9f54bc4bc719ac031d
Ghetsa <ghetsa.arryanti@gmail.com>
2023-10-30 22:30:45 +0700

    push BioskopWithScanner12.java sebelum modif

---
+++ jobsheet10/BioskopWithScanner12.java
@@ -0,0 +1,34 @@
+package jobsheet10;
+import java.util.Scanner;
+
+public class BioskopWithScanner12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int baris, kolom;
+        String nama, next;
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan Nama: ");
+            nama = sc.nextLine();
+            
+            System.out.print("Masukkan Baris: ");
+            baris = sc.nextInt();
+            
+            System.out.print("Masukkan Kolom: ");
+            kolom = sc.nextInt();
+            
+            penonton[baris - 1][kolom - 1] = nama;
+            sc.nextLine();
+
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+
+    }
+}
