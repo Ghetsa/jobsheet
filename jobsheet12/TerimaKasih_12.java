d08e941e8f6203e3f4ae0063de15d0fab174efab
Ghetsa <ghetsa.arryanti@gmail.com>
2023-11-20 20:27:31 +0700

    add: terimakasih.java sebelum modif

---
+++ jobsheet12/TerimaKasih_12.java
@@ -0,0 +1,23 @@
+package jobsheet12;
+
+public class TerimaKasih_12 {
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" +
+        "You inspired in me a love for learning and made me feel like I could ask you anything."
+        );
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Bapak..... Ibu.... Semoga sehat selalu";
+        UcapanTambahan(ucapan);
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
+
+
+    
+}
